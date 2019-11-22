5d35dee4c38f4408831aeee38fa756945c56d43b
Wright-Dj <wrightdj2000@gmail.com>
2019-11-22 08:11:53 -0800

    Add files via upload

---
+++ js/arelements.js
@@ -0,0 +1,32 @@
+AFRAME.registerComponent('box', {
+    schema: {
+      width: {type: 'number', default: 1},
+      height: {type: 'number', default: 1},
+      depth: {type: 'number', default: 1},
+      color: {type: 'color', default: '#AAA'}
+    },
+  
+    /**
+     * Initial creation and setting of the mesh.
+     */
+    init: function () {
+      var data = this.data;
+      var el = this.el;
+      console.log("done");
+  
+      // Create geometry.
+      this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
+  
+      // Create material.
+      this.material = new THREE.MeshStandardMaterial({color: data.color});
+  
+      // Create mesh.
+      this.mesh = new THREE.Mesh(this.geometry, this.material);
+  
+      // Set mesh on entity.
+      el.setObject3D('mesh', this.mesh);
+    },
+    tick: function(){
+
+    },
+  });
